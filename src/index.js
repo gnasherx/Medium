5e5b6f2a1101fb0d606fcad91a8c2e56ad48e138
gnasherx <pawarg124@gmail.com>
2017-11-10 10:35:51 +0530

    add config for deveolpment

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 /* eslint-disable no-console */
 import express from 'express';
 
+import constants from './config/constants';
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, err => {
-    if (err) {
-        throw err;
-    } else {
-        console.log(`
+app.listen(constants.PORT, err => {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`
             Server is running on port: ${PORT}
             ----
             Running on ${process.env.NODE_ENV}
         `);
-    }
+  }
 });
