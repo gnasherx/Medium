733eac545d7128bed6641bd979dd8c635f5c4f5b
gnasherx <pawarg124@gmail.com>
2017-11-10 09:56:10 +0530

    add nodemon

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,12 @@ module.exports = {
     target: "node",
     externals: [nodeExternals()],
     entry: {
-        'index': './src/index/js',
+        'index': './src/index.js',
     },
     output: {
         path: path.join(__dirname, 'dist'),
         filename: '[name].bundle.js',
-        librarytTarget: 'commonjs2',
+        libraryTarget: 'commonjs2',
     },
     module: {
         rules: [
